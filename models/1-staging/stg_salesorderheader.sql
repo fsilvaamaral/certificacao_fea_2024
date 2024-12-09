54028aec3493e7d966b3865eaf6233994fd9a4a7
Felipe Amaral <fsaeconomia@gmail.com>
2024-12-09 19:57:47 +0000

    Finalizada a int_vendas e sua documentação

--- models/1-staging/stg_salesorderheader.sql
+++ models/1-staging/stg_salesorderheader.sql
@@ -9,7 +9,15 @@ with
             cast (SALESORDERID as int) as id_pedido
             , cast (ORDERDATE as date) as data_pedido
             , cast (SHIPDATE as date) as data_envio_pedido
-            , cast (STATUS as int) as status_pedido
+            , case
+                when STATUS = 1 then 'Pedido em processamento'
+                when STATUS = 2 then 'Pedido aprovado'
+                when STATUS = 3 then 'Pedido em espera'
+                when STATUS = 4 then 'Pedido rejeitado'
+                when STATUS = 5 then 'Pedido enviado'
+                when STATUS = 6 then 'Pedido cancelado'
+                else 'Sem Status'
+            end as status_pedido
             , case 
                 when ONLINEORDERFLAG = true then 'Loja Online'
                 else 'Loja Física'
